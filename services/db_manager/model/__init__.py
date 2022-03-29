b7fc8330466f937660c1b946cbb94d93c8ead634
dgtrv <degtiarev@outlook.com>
2022-03-29 14:26:07 +0300

    fixed db address in __init__.py of model

--- services/db_manager/model/__init__.py
+++ services/db_manager/model/__init__.py
@@ -3,5 +3,5 @@ from sqlalchemy.orm import sessionmaker
 
 from .model import Base, Hero, Side, Story, Motto, Interaction
 
-engine = create_engine('postgresql://dgtrv:dgtrv@188.68.222.47:5432/heroes_db')
+engine = create_engine('postgresql://dgtrv:dgtrv@db:5432/heroes_db')
 Session = sessionmaker(bind=engine)
