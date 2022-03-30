44f11fbe33fdf073750876163db64b3bd6b03c44
dgtrv <degtiarev@outlook.com>
2022-03-31 02:59:55 +0300

    log rotation

--- services/db_manager/logger/common_log.py
+++ services/db_manager/logger/common_log.py
@@ -1,12 +1,11 @@
 import logging
+from logging.handlers import RotatingFileHandler
 from logger import LOG_LEVEL, LOG_FILE
 
 logging.basicConfig(
     format='%(asctime)s: %(name)-12s: %(levelname)-8s: %(message)s',
     datefmt='%Y.%m.%d %H:%M:%S',
-    filename=LOG_FILE,
-    filemode='a',
-    level=logging.getLevelName(LOG_LEVEL)
+    level=logging.INFO
 )
 
 logging.info('Log session started')
@@ -20,3 +19,11 @@ formatter = logging.Formatter('%(name)-12s: %(levelname)-8s: %(message)s')
 console.setFormatter(formatter)
 logging.getLogger('').addHandler(console)
 
+file_handler_with_rotation = RotatingFileHandler(
+    filename=LOG_FILE,
+    mode='a',
+    maxBytes=5e6,
+    backupCount=10
+)
+file_handler_with_rotation.setLevel(logging.getLevelName(LOG_LEVEL))
+logging.getLogger('').addHandler(file_handler_with_rotation)
