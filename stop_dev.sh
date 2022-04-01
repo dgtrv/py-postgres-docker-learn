0ee6f07c0e974df9edfb69c7e69b3df35fe5a8d2
dgtrv <degtiarev@outlook.com>
2022-04-01 23:26:46 +0300

    fixed bug in stop_dev.sh

--- stop_dev.sh
+++ stop_dev.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 echo "Shutting down development..."
-exec docker-compose -f docker-compose.yml --env-file .env down -v 
+exec docker-compose -f docker-compose.yml --env-file .env.dev down -v 
 
